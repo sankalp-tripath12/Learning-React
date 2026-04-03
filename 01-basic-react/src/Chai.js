a659bf4c98d8acde85574eaa84f3abd117009098
sankalp <tripathisachin286@gmail.com>
2026-04-03 21:00:29 +0530

    cleaned project and added chai components

---
+++ 01-basic-react/src/Chai.js
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h2>chai in react with coffee</h2>
+    )
+}
+export default Chai
---
+++ 02-vite-react/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h1>code aur chai in the </h1>
+    )
+}
+export default Chai
